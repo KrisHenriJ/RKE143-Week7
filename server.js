e611319714ec25a18d9b86b7c0b15b2c017dc26f
Kris Henri Jarvi <Kris.henri.jarvi@tktk.ee>
2026-05-09 22:27:24 +0300

    Add pumpkin and rke143 routes with authentication logic

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
 const express = require('express');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const app = express();
 
 const pumpkinData = require('./data/pumpkin.json');
 const broccoliData = require('./data/broccoli.json');
-const rkeData = require('./data/nodejs.json');
+const nodejsData = require('./data/nodejs.json');
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) =>{
-    const randomIndex = Math.floor(Math.random() * pumpkinData.Categories['Pumpkin Drinks'].length);
+app.get('/', (req, res) => {
+    const randomIndex = Math.floor(Math.random()* pumpkinData.Categories['Pumpkin Drinks'].length);
     const randomDrinkRecipe = pumpkinData.Categories['Pumpkin Drinks'][randomIndex];
-
-    res.status(200).json({randomDrinkRecipe});
     
+    res.status(200).json({randomDrinkRecipe});
 });
 
-app.get('/pumpkin', (req, res) => {
-      res.status(200).json({message:'Got pumpkin?'});
+app.get('/pumpkin', (req, res) =>{
+    res.status(200).json({message: 'Got pumpkin?'});
 });
 
-app.post('/pumpkin', (req, res) => {
+app.post('/pumpkin', (req, res) =>{
     console.log(req.body);
-
     if(req.body.username !== 'admin' || req.body.password !== "1234"){
         res.status(200).json({broccoliData});
-    } else {
+    } 
+    else{
         res.status(200).json({pumpkinData});
     }
 });
 
-app.post('/rke143', (req, res) => {
+app.post('/rke143', (req, res) =>{
     console.log(req.body);
-
     if(req.body.name !== 'rke' || req.body.code !== "143"){
-        res.status(200).json({message: 'Invalid credentials'});
-    } else {
-        res.status(200).json({rkeData});
+        res.status(200).json({message: 'Invalid credentials.'});
+    } 
+    else{
+        res.status(200).json({nodejsData});
     }
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000.');
 });
